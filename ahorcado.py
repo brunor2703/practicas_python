79287abe74a3dd7fdf465d12d5f12b44b6de66e8
brunor2703 <enriqueluevano20@gmail.com>
2023-05-19 13:05:47 -0600

    juego del ahorado2

--- ahorcado.py
+++ ahorcado.py
@@ -1,7 +1,16 @@
 board = [" -----", "     |", "     |", "     |", "     |", "     |", "------" ]
+pasos = (("|",1,1), ("o",1,2), ("\\",0,2), ("/",2,2),("|",1,3),("/",0,4),("\\",2,4))
 
 def drawboard():
     for i in board:
         print(i)
 
+def testboard():
+    for paso in pasos:
+        caracter = paso[0]
+        x = paso[1]
+        y = paso[2]
+        print (caracter, x, y)
+
 drawboard()
+testboard()
