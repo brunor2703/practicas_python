1f771368e9be53a90e74657859060aab089baa56
brunor2703 <enriqueluevano20@gmail.com>
2023-05-19 13:17:09 -0600

    juego del ahorcado3

--- ahorcado.py
+++ ahorcado.py
@@ -2,15 +2,29 @@ board = [" -----", "     |", "     |", "     |", "     |", "     |", "------" ]
 pasos = (("|",1,1), ("o",1,2), ("\\",0,2), ("/",2,2),("|",1,3),("/",0,4),("\\",2,4))
 
 def drawboard():
+    """
+    Dibuja el tablero
+    """
     for i in board:
         print(i)
 
+def drawpaso(numpaso):
+    """
+    Coloca en el tablero el paso indicado
+    """
+    paso = pasos[numpaso]
+    caracter = paso[0]
+    x = paso[1]
+    y = paso[2]
+    board[y] = board[y][:x] + caracter + board[y][x+1:]
+
 def testboard():
-    for paso in pasos:
-        caracter = paso[0]
-        x = paso[1]
-        y = paso[2]
-        print (caracter, x, y)
+    """
+    Prueba el tablero completando los pasos del ahorcado
+    """
+    for numpaso in range(len(pasos)):
+        drawpaso(numpaso)
 
 drawboard()
 testboard()
+drawboard()
